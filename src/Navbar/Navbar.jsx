593ed1132dac2fa47170892817647861ea10cf3c
Elnaraa9 <7khtht3@code.edu.az>
2023-05-02 19:28:09 +0400

    first commit

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar__header'>
+            <div> <p className='textt'>MYLOGO</p></div>
+     
+      <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Portfolio</li>
+      <li>Pricing</li>
+      <li>Contocts</li>
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
